35569fbdcd3124dd591ce83fa40030a9c9e45ca5
Kathryn Lovett <lovett2@uoregon.edu>
2016-07-18 15:29:59 -0700

    Put file name variable in double quotes again.

--- modules/apache/manifests/init.pp
+++ modules/apache/manifests/init.pp
@@ -14,7 +14,7 @@ class apache {
 		$package: ensure => installed; 
 	}
 
-	file { 'apacheconf':
+	file { "$apacheconf":
 		source  => [
 			# from modules/apache2/files/apache2.conf
 			"puppet:///modules/apache2/apache2.conf",
