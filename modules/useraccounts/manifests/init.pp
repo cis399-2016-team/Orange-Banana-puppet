5f269e7c7312ae0ddb56200d88b3eca2e49dedcf
Kathryn Lovett <lovett2@uoregon.edu>
2016-07-21 17:23:41 -0700

    Forgot to change user from ubuntu for one ssh key. Added stevev ssh key.

--- modules/useraccounts/manifests/init.pp
+++ modules/useraccounts/manifests/init.pp
@@ -70,6 +70,12 @@ class useraccounts {
 		managehome => true,
 	}
 
+	ssh_authorized_key { "stevev-key-pair-oregon":
+    	user => "stevev",
+		type => "ssh-rsa",
+		key  => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCrFq80b0ptexNiI6KP4hxww5d5RFm8djIpsdJqRZDyoyD5vaf7d30bTLef8su6stHuBBjKccMcUjNyu4BliJBXIy7bKVDllVB5oeLFizDahQcgqjYfzyqj16uEa7NLBW5/3ljLpPX8XEI7YFM/hg65JFgpQIAiBi2N6bGj9mQrh/51SpCO6FruQH8KVjDl/CLgbnFq9cDwRDAo4tvPO1b0MRVrvM8BbZbBUHqV/093jVXkwY+BxsU6cgOnHrSmoTnH4MqMXUI/ok31JORVbWW5NAz28Ag7V/NbDvRBIYicJOd9aqEST/L812+tmnE8iQzn3bZvv7v0E7FHneCS5Qpz",
+	}
+
 	ssh_authorized_key { "chenders-key-pair-oregon":
     	user => "chenders",
 		type => "ssh-rsa",
@@ -165,7 +171,7 @@ class useraccounts {
 
 
 	ssh_authorized_key { "clwaring":
-		user => "ubuntu",
+		user => "clwaring",
 		type => "ssh-rsa",
 		key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCoamIc24g51gBC55Bbcq8AUi9ghA5Y7l3b8mnOQvX+kQJpfNt//cMbUXAhBMnjiv+cfFKi+klW8UUSI1SPccpV/nGbZH/k9XEKnpqK3XBRPUEHbA5QGujqOW7J44zjYKeXovH6PwQvZepjENI6ti3nBX7bvd8CRlMK3B3U4IJtghF8mz4FVLDjx1OOQAEDv5trzd7F0fRnWiuDVH9sUsVAeziUWhYKVmz9AhgSnnx8JSHXw6Rn5PGZyWDCttd0vG1KMPGULjc4nK5Oru3cu2Hsgb0dN0AjxbOwAamEto98zOe4HkO6oP71tOhk0KQmVKNgP/SpQ7Lh9gkTp2difDs/",
 	}
